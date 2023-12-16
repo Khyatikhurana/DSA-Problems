50f767c4999457741a7ab68f84ba6369610feebc
Khyati Khurana <120809253+Khyatikhurana@users.noreply.github.com>
2023-12-17 00:33:16 +0530

    Time: 28 ms (18.84%), Space: 13.3 MB (54.69%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        index=-1 #break point
+        n=len(nums)
+        # Step 1: Find the break point:
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                index=i #i is the break point
+                break
+        
+        #if break point does not exist
+        if index==-1:
+            nums.reverse()
+            return nums #we get the first combination
+        
+        for i in range(n-1,index,-1):
+            if nums[i]>nums[index]:
+                nums[i],nums[index]=nums[index],nums[i]
+                break
+        
+        #reverse right hand side
+        nums[index+1:]=reversed(nums[index+1:])
+        
+        return nums
+        
+        
+        
+        
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
