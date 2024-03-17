7e7c44a82a46d9a9bc5e3b9e20eb98251112bb88
Khyati Khurana <120809253+Khyatikhurana@users.noreply.github.com>
2024-03-17 23:28:45 +0530

    Time: 1 ms (74.71%), Space: 45.5 MB (62.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -21,6 +21,7 @@ class Solution {
                 }
              }
         }
+        
         //traverse matrix-(row and col) to mark zero
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
