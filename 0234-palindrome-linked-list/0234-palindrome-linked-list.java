20107fe79653e9213e7158cd988abeca8223a4ed
Khyati Khurana <120809253+Khyatikhurana@users.noreply.github.com>
2024-05-29 10:42:05 +0530

    Time: 9 ms (26.82%), Space: 71.2 MB (6.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,48 +11,42 @@
 class Solution {
     
     public ListNode getMiddle(ListNode head){
-        //using hare and turtle method
-        ListNode H = head;
-        ListNode T = head;
-        while(H.next != null && H.next.next != null){
-            H = H.next.next;
-            T = T.next;
+        ListNode hare = head;
+        ListNode turtle = head;
+        while(hare.next != null && hare.next.next != null){
+            hare = hare.next.next;
+            turtle = turtle.next;
         }
-        return T;
-        
+        return turtle;    
     }
     
     public ListNode reverse(ListNode head){
-        ListNode prev = head;
-        ListNode curr = head.next;
-        while(curr != null){
-            ListNode next = curr.next;
-            //reverse
-            curr.next = prev;
-            //update
-            prev = curr;
-            curr = next;
-            
-            
+        if(head == null || head.next == null){
+            return head;
         }
-        head.next = null; //removing forward link
-        head = prev;
-        return prev;
+        
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
+    
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null){
             return true;
         }
         
         ListNode middle = getMiddle(head); //last elem of first half
-        ListNode secondHalfStart = reverse(middle.next);
-        ListNode firstHalfStart = head;
-        while(secondHalfStart != null){
-            if(firstHalfStart.val != secondHalfStart.val){
+        ListNode secondHead = reverse(middle.next);
+        ListNode firstHead = head;
+        
+        while(secondHead != null){
+            if(secondHead.val != firstHead.val){
                 return false;
             }
-            firstHalfStart = firstHalfStart.next;
-            secondHalfStart = secondHalfStart.next;
+            
+            secondHead = secondHead.next;
+            firstHead = firstHead.next;
         }
         return true;
         
