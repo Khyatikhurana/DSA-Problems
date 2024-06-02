b1caf0066c1d56bff6b1ac79f0d024ee880488ae
Khyati Khurana <120809253+Khyatikhurana@users.noreply.github.com>
2024-06-03 00:31:20 +0530

    Time: 1 ms (99.90%), Space: 44.7 MB (21.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -9,16 +9,19 @@ class Solution {
         }
         
         if(arr[idx] <= target){
+            
             ds.add(arr[idx]);
+            //pick, not idx +1 because duplicate elements allowed
             findComb(idx,arr, target - arr[idx], ansList,ds);
             ds.remove(ds.size()-1);
         }
+        //do not pick
         findComb(idx+1, arr, target, ansList, ds);
         
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ansList = new ArrayList<>();
-        findComb(0,candidates, target, ansList, new ArrayList<>());
+        List<List<Integer>> ansList = new ArrayList<>(); //final ds
+        findComb(0,candidates, target, ansList, new ArrayList<>()); //empty data structure
         return ansList;
         
     }
